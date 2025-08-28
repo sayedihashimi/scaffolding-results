688d272a1535ffb22708160d98eb0844ce367016
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2025-08-28 14:38:17 -0400

    scaffolding contact, add migration and update database

---
+++ src/WebApiControllers/Data/WebApiControllersContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApiControllers.Models;
+
+namespace WebApiControllers.Data
+{
+    public class WebApiControllersContext : DbContext
+    {
+        public WebApiControllersContext (DbContextOptions<WebApiControllersContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApiControllers.Models.Contact> Contact { get; set; } = default!;
+    }
+}
