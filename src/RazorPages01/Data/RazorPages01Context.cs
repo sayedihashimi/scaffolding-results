a08c92fe9fe9af371dce8853194e1e014617bf20
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2025-08-28 14:18:15 -0400

    scaffolding contact, add migration and update database

---
+++ src/RazorPages01/Data/RazorPages01Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RazorPages01.Models;
+
+namespace RazorPages01.Data
+{
+    public class RazorPages01Context : DbContext
+    {
+        public RazorPages01Context (DbContextOptions<RazorPages01Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RazorPages01.Models.Contact> Contact { get; set; } = default!;
+    }
+}
