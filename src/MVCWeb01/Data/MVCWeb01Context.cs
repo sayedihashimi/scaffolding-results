ef733b1a487bb739c7623ec7edcb1083c003fddd
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2025-08-28 14:22:47 -0400

    scaffolding contact, add migration and update database

---
+++ src/MVCWeb01/Data/MVCWeb01Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MVCWeb01.Models;
+
+namespace MVCWeb01.Data
+{
+    public class MVCWeb01Context : DbContext
+    {
+        public MVCWeb01Context (DbContextOptions<MVCWeb01Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MVCWeb01.Models.Contact> Contact { get; set; } = default!;
+    }
+}
